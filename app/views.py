051cff2ecc7cb01cd330c8c106728526271489c3
shubhammahawar1 <shubhamm033@gmail.com>
2024-10-03 17:38:01 +0530

    adding locking to watch video api

--- app/views.py
+++ app/views.py
@@ -5,7 +5,10 @@ from .serializers import VideoSerializer, WatchVideoSerializer, UserSerializer, 
 from rest_framework.response import Response
 from rest_framework import status
 from .models import VideoViews, User, Video
+import threading
+from datetime import datetime
 
+lock = threading.Lock()
 
 # Create your views here.
 @api_view(['POST'])
@@ -51,14 +54,18 @@ def fetch_video_details(request, id):
 
 @api_view(["POST"])
 def watch_video(request):
+    start_time = datetime.now()
     serializer = WatchVideoSerializer(data=request.data)
     if serializer.is_valid():
         try:
+            lock.acquire()
             video_views = VideoViews.objects.get(video=serializer.validated_data["video"])
             video_views_count = video_views.count + 1
             video_views.count = video_views_count
             video_views.save()
-            return Response({"success": "tv watching"}, status=status.HTTP_201_CREATED)
+            lock.release()
+            return Response({"success": "tv watching",
+                             "response_time" : (datetime.now() - start_time).microseconds}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({"error": serializer.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
